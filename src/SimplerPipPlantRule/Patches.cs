b16fde2cf77bc126103f15f334b1115fc27b58aa
limc <ancienlee@qq.com>
2021-04-24 10:44:03 +0800

    [refactor]introduce VLib

--- src/SimplerPipPlantRule/Patches.cs
+++ src/SimplerPipPlantRule/Patches.cs
@@ -1,48 +1,38 @@
 ﻿using System.Linq;
+using AsLimc.commons;
 using Harmony;
 using PeterHan.PLib;
 using PeterHan.PLib.Options;
 using UnityEngine;
 
-namespace MightyVincent
-{
-    internal static class Patches
-    {
-        public static Settings settings;
-
-        public static void OnLoad()
-        {
+namespace AsLimc.SimplerPipPlantRule {
+    internal static class Patches {
+        public static void OnLoad() {
             PUtil.InitLibrary();
             POptions.RegisterOptions(typeof(Settings));
-            Debug.Log("Loading settings");
-            settings = POptions.ReadSettings<Settings>() ?? new Settings();
+            VLib.Init();
+            Settings.Init();
         }
 
         [HarmonyPatch(typeof(SeedPlantingMonitor.Def), MethodType.Constructor)]
-        internal class SeedPlantingMonitor_Def_Constructor
-        {
-            public static void Postfix(ref float ___searchMinInterval, ref float ___searchMaxInterval)
-            {
-                ___searchMinInterval = Patches.settings.SearchMinInterval;
-                ___searchMaxInterval = Patches.settings.SearchMaxInterval;
+        internal class SeedPlantingMonitor_Def_Constructor {
+            public static void Postfix(ref float ___searchMinInterval, ref float ___searchMaxInterval) {
+                ___searchMinInterval = Settings.Get().SearchMinInterval;
+                ___searchMaxInterval = Settings.Get().SearchMaxInterval;
             }
         }
 
         [HarmonyPatch(typeof(PlantableCellQuery), MethodType.Constructor)]
-        internal class PlantableCellQuery_Constructor
-        {
-            public static void Postfix(ref int ___plantDetectionRadius, ref int ___maxPlantsInRadius)
-            {
-                ___plantDetectionRadius = Patches.settings.PlantDetectionRadius;
-                ___maxPlantsInRadius = Patches.settings.MaxPlantsInRadius;
+        internal class PlantableCellQuery_Constructor {
+            public static void Postfix(ref int ___plantDetectionRadius, ref int ___maxPlantsInRadius) {
+                ___plantDetectionRadius = Settings.Get().PlantDetectionRadius;
+                ___maxPlantsInRadius = Settings.Get().MaxPlantsInRadius;
             }
         }
 
         [HarmonyPatch(typeof(PlantableCellQuery), "CountNearbyPlants")]
-        internal class PlantableCellQuery_CountNearbyPlants
-        {
-            public static bool Prefix(int cell, int radius, out int __result)
-            {
+        internal class PlantableCellQuery_CountNearbyPlants {
+            public static bool Prefix(int cell, int radius, out int __result) {
 //                Debug.Log($"CountNearbyPlants Prefix: {cell}, {radius}");
                 Grid.CellToXY(cell, out var x, out var y);
                 var side = radius * 2 + 1;
--- src/SimplerPipPlantRule/Settings.cs
+++ src/SimplerPipPlantRule/Settings.cs
@@ -1,24 +1,34 @@
 using Newtonsoft.Json;
 using PeterHan.PLib;
+using PeterHan.PLib.Options;
 
-namespace MightyVincent
+namespace AsLimc.SimplerPipPlantRule
 {
+    [ModInfo("AsLimc.HyperReservoir.LocStrings.Settings.MOD_NAME", "https://github.com/as-limc/oni-mods", collapse: true)]
+    [RestartRequired]
     [JsonObject(MemberSerialization.OptIn)]
-    public class Settings
+    internal class Settings
     {
-        [Option("Search Min Interval (seconds)", "min interval for seed search. (Restart Needed)", null)]
+        private static Settings _Instance;
+        public static Settings Get() => _Instance;
+
+        public static void Init() {
+            _Instance ??= POptions.ReadSettings<Settings>() ?? new Settings();
+        }
+        
+        [Option("Search Min Interval (seconds)", "min interval for seed search.", null)]
         [JsonProperty]
         public float SearchMinInterval { get; set; }
 
-        [Option("Search Max Interval (seconds)", "max interval for seed search. (Restart Needed)", null)]
+        [Option("Search Max Interval (seconds)", "max interval for seed search.", null)]
         [JsonProperty]
         public float SearchMaxInterval { get; set; }
 
-        [Option("Plant Detection Radius (tiles)", "detection area is a square area with 'plantDetectionRadius * 2 + 1' length of the sides and centered at the target tile. (Restart Needed)", null)]
+        [Option("Plant Detection Radius (tiles)", "detection area is a square area with 'plantDetectionRadius * 2 + 1' length of the sides and centered at the target tile.", null)]
         [JsonProperty]
         public int PlantDetectionRadius { get; set; }
 
-        [Option("Max Plants In Radius (tiles)", "max allowed plants in detection area of the target tile is 'maxPlantsInRadius + 1'. (Restart Needed)", null)]
+        [Option("Max Plants In Radius (tiles)", "max allowed plants in detection area of the target tile is 'maxPlantsInRadius + 1'.", null)]
         [JsonProperty]
         public int MaxPlantsInRadius { get; set; }
 
